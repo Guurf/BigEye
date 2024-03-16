912275fc7f196563b5b8f44c51f0b60e8d14bfb2
Guurf <102306125+Guurf@users.noreply.github.com>
2024-03-16 17:10:40 +0000

    player movement + gun animations

---
+++ Gun/gun.gd
@@ -0,0 +1,40 @@
+extends Node3D
+@onready var hand = $"../Head/Camera3D/Hand"
+@onready var camera = $"../Head/Camera3D"
+@onready var animation = $pistol/AnimationPlayer
+@onready var animation_tree = $pistol/AnimationTree
+@onready var player = $".."
+
+func _ready():
+	animation_tree.active = true
+
+func _process(delta):
+	global_position.x = lerp(global_position.x, hand.global_position.x, 1)
+	global_position.y = hand.global_position.y
+	global_position.z = hand.global_position.z
+	rotation.x = camera.rotation.x
+	update_animation_parameters()
+
+func update_animation_parameters():
+	if (player.speed == player.SPRINT_SPEED):
+		animation_tree["parameters/conditions/is_running"] = true
+		animation_tree["parameters/conditions/idle"] = false
+		animation_tree["parameters/conditions/is_walking"] = false
+	elif Input.get_vector("left", "right", "up", "down") != Vector2.ZERO:
+		animation_tree["parameters/conditions/is_walking"] = true
+		animation_tree["parameters/conditions/is_running"] = false
+		animation_tree["parameters/conditions/idle"] = false
+	else:
+		animation_tree["parameters/conditions/idle"] = true
+		animation_tree["parameters/conditions/is_walking"] = false
+		animation_tree["parameters/conditions/is_running"] = false
+	
+	if (Input.is_action_just_pressed("shoot")):
+		animation_tree["parameters/conditions/shoot"] = true
+	else:
+		animation_tree["parameters/conditions/shoot"] = false
+	
+	if (Input.is_action_pressed("reload")):
+		animation_tree["parameters/conditions/reload"] = true
+	else:
+		animation_tree["parameters/conditions/reload"] = false
